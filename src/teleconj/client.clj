19ab0c605f72d1a9878443d62e25af7a4dc000cc
Luis Guilherme Coelho <lgcoelho@disroot.org>
2023-12-22 14:56:54 -0300

    style: improve readability

--- src/teleconj/client.clj
+++ src/teleconj/client.clj
@@ -5,14 +5,19 @@
 
 (defn- call-api-method [method token params]
   (let [url (str base-url "/bot" token "/" method)]
-    (client/post url {:content-type :json :form-params params})))
+    (client/post url {:content-type
+                      :json
+                      :form-params
+                      params})))
 
 (defn send-message
   ([token chat-id text]
    (send-message token chat-id text nil))
   ([token chat-id text params]
    (let [url (str base-url "/bot" token "/sendMessage")
-         form (conj {:chat_id chat-id :text text} params)]
+         form (conj {:chat_id chat-id
+                     :text text}
+                     params)]
      (call-api-method "sendMessage" token form))))
 
 (defn forward-message
@@ -20,5 +25,8 @@
    (forward-message token chat-id from-chat-id message-id nil))
   ([token chat-id from-chat-id message-id params]
    (let [url (str base-url "/bot" token "/sendMessage")
-         form (conj {:chat_id chat-id :from_chat_id from-chat-id :message_id message-id} params)]
+         form (conj {:chat_id chat-id
+                     :from_chat_id from-chat-id
+                     :message_id message-id}
+                    params)]
      (call-api-method "forwardMessage" token form))))
