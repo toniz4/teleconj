405c88ab905214ab62958fa8f597fa08bfeac941
Luis Guilherme Coelho <lgcoelho@disroot.org>
2023-12-22 15:16:19 -0300

    style: remove unused local variable

--- src/teleconj/client.clj
+++ src/teleconj/client.clj
@@ -14,8 +14,7 @@
   ([token chat-id text]
    (send-message token chat-id text nil))
   ([token chat-id text params]
-   (let [url (str base-url "/bot" token "/sendMessage")
-         form (conj {:chat_id chat-id
+   (let [form (conj {:chat_id chat-id
                      :text text}
                      params)]
      (call-api-method "sendMessage" token form))))
@@ -24,8 +23,7 @@
   ([token chat-id from-chat-id message-id]
    (forward-message token chat-id from-chat-id message-id nil))
   ([token chat-id from-chat-id message-id params]
-   (let [url (str base-url "/bot" token "/sendMessage")
-         form (conj {:chat_id chat-id
+   (let [form (conj {:chat_id chat-id
                      :from_chat_id from-chat-id
                      :message_id message-id}
                     params)]
